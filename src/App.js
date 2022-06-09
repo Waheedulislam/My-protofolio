cc992f15a764fc6231f8908a0ffdcf4e3517d893
Waheedul Islam <waheeduli81@gmail.com>
2022-06-10 01:07:42 +0600

    cv-download

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Intro from './Coponents/Intro/Intro';
 import Navbar from './Coponents/Navbar/Navbar';
+import Services from './Coponents/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Intro></Intro>
+      <Services></Services>
     </div>
   );
 }
