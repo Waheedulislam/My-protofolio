2ca2513930e8b2900c726ec94f9b089cb2f56817
Waheedul Islam <waheeduli81@gmail.com>
2022-06-09 23:05:21 +0600

    home

--- src/Coponents/Intro/Intro.js
+++ src/Coponents/Intro/Intro.js
@@ -9,11 +9,13 @@ import boy from '../../img/boy.png'
 import thumbup from '../../img/thumbup.png'
 import crown from '../../img/crown.png'
 import glassesimoji from '../../img/glassesimoji.png'
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 const Intro = () => {
     return (
         <div className="intro">
             <div className="i-left">
+
                 <div className="i-name">
                     <span>Hy! I am </span>
                     <span>Waheedul Islam</span>
@@ -32,9 +34,23 @@ const Intro = () => {
 
 
             <div className="i-right">
+
                 <img src={Vector1} alt="" />
                 <img src={Vector2} alt="" />
                 <img src={boy} alt="" />
+                <img src={glassesimoji} alt="" />
+
+                <div style={{ top: '10rem', left: '57rem' }}>
+                    <FloatingDiv image={crown} txt1='Web' txt2='Developer'></FloatingDiv>
+                </div>
+
+                <div style={{ marginTop: '19rem', left: '34rem' }}>
+                    <FloatingDiv image={thumbup} txt1='Best Design' text2='Award' />
+                </div>
+            </div>
+
+            <div className='blur' style={{ background: 'rgb(236 210 255)' }}>
+
             </div>
         </div>
     );
