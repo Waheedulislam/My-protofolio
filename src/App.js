da5867c9337db3964e3320e456c5597b23bb1e4c
Whaeedil <baoppyhossen1234@gmail.com>
2022-06-10 18:57:52 +0600

    website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Experience from './Coponents/Experience/Experience';
 import Intro from './Coponents/Intro/Intro';
 import Navbar from './Coponents/Navbar/Navbar';
+import Protoflio from './Coponents/Protoflio/Protoflio';
 import Services from './Coponents/Services/Services';
+import Works from './Coponents/Works/Works';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar></Navbar>
       <Intro></Intro>
       <Services></Services>
+      <Experience></Experience>
+      <Works></Works>
+      <Protoflio></Protoflio>
     </div>
   );
 }
