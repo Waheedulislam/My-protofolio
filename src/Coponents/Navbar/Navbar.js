09489400e92105af0b187f05670d16bfd9c69997
Waheedul Islam <waheeduli81@gmail.com>
2022-06-11 23:33:37 +0600

    done

--- src/Coponents/Navbar/Navbar.js
+++ src/Coponents/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Navbar/Navbar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -11,20 +12,20 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
-                            <li><a>About</a></li>
-                            <li><a>Blogs</a></li>
-                            <li><a>Contact</a></li>
+                            <li ><Link to='/'>Home</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/blogs'>Blogs</Link></li>
+                            <li><Link to='/contact'>Contact</Link></li>
                         </ul>
                     </div>
-                    <a class="btn btn-ghost normal-case text-xl">My <span className=' text-orange-400 text-xl pl-2'>Portfolio</span></a>
+                    <Link to='/' class="btn btn-ghost normal-case text-xl">My <span className=' text-orange-400 text-xl pl-2'>Portfolio</span></Link >
                 </div>
                 <div class="navbar-center hidden lg:flex">
                     <ul class="menu menu-horizontal p-0">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Blogs</a></li>
-                        <li><a>Contact</a></li>
+                        <li ><Link to='/'>Home</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><Link to='/blogs'>Blogs</Link></li>
+                        <li><Link to='/contact'>Contact</Link></li>
                     </ul>
                 </div>
             </div>
