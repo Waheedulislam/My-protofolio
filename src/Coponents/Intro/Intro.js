ed6b7e3d94e111cc7ae5ece842bd19dd95ed5f99
Waheedul Islam <waheeduli81@gmail.com>
2024-06-09 18:55:26 +0600

    okh

--- src/Coponents/Intro/Intro.js
+++ src/Coponents/Intro/Intro.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import '../Intro/Intro.css'
-import Github from '../../img/github.png'
-import LinkedIn from '../../img/linkedin.png'
-import Instragram from '../../img/instagram.png'
-import my from '../../img/my (3).jpg'
-import Resume from './Wahidul Islam (4).pdf'
+import React from "react";
+import "../Intro/Intro.css";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instragram from "../../img/instagram.png";
+import my from "../../img/my (3).jpg";
+import Resume from "./Wahidul Islam (4).pdf";
 
 const Intro = () => {
-    return (
-        <div>
-            <div class="hero min-h-screen ">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img src={my} class="max-w-96 max-h-96 rounded-lg shadow-2xl" />
+  return (
+    <div>
+      <div class="hero min-h-screen ">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={my} class="max-w-96 max-h-96 rounded-lg shadow-2xl" />
 
-                    <div className='max-w-16'>
-                        <div className="i-name">
-                            <span className='font-bold '>Hy! I am </span>
-                            <span >Waheedul Islam</span>
-                            <span>Fronted Developer with high level and development, producting the Quality work</span>
-                        </div>
-                        <a href={Resume} download>
-                            <button className='button s-button'>Download CV</button>
-                        </a>
-
-                        <div className="i-icon">
-
-                            <img src={Github} alt="" />
-                            <img src={LinkedIn} alt="" />
-                            <img src={Instragram} alt="" />
-                        </div>
-                    </div>
-                </div>
+          <div className="max-w-16">
+            <div className="i-name">
+              <span className="font-bold ">Hello s! I am </span>
+              <span>Waheedul Islam</span>
+              <span>
+                Fronted Developer with high level and development, producting
+                the Quality work
+              </span>
             </div>
-        </div>
+            <a href={Resume} download>
+              <button className="button s-button">Download CV</button>
+            </a>
 
-    );
+            <div className="i-icon">
+              <img src={Github} alt="" />
+              <img src={LinkedIn} alt="" />
+              <img src={Instragram} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
